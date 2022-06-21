116db0e63ae03d67e607271c193a3340d7913d7d
Rafael Souza Nascimento <83884395+RafaSouz@users.noreply.github.com>
2022-06-21 20:18:07 -0300

    Update const.py

--- const.py
+++ const.py
@@ -1,5 +1,5 @@
-CHAT_SERVER_HOST = "10.128.0.3"
+CHAT_SERVER_HOST = "100.26.29.167"
 CHAT_SERVER_PORT = 5001
 
 # IDs de usuários registrados com os respectivos endereços IP e números de porta
-registry = {"Alice":("10.128.0.2",5002), "Bob":("10.128.0.4",5002)}
+registry = {"Alice":("54.92.213.109",5002), "Bob":("54.82.106.183",5002), "Charlie":("34.204.17.108",5002)}
